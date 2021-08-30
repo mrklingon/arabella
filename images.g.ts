54e9030cfbf7e15263e508237c60abf00f3014ea
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-08-29 22:33:15 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "ship":return img`
+. . 5 5 . . . . . . . . . . . . 
+. 5 e 5 5 5 5 . . . . . . . . . 
+. 5 e 5 5 5 5 5 5 5 . . . . . . 
+. 5 e 5 5 5 5 5 5 5 5 5 . . . . 
+. 5 e 5 5 e 5 5 5 5 5 e . . . . 
+. 5 5 e 5 5 e 5 5 e 5 5 e . . . 
+. 5 5 e 5 5 e 5 5 5 e 5 5 e . . 
+. 5 5 e 5 5 5 e 5 5 e 5 5 e . . 
+. . 5 e 5 5 5 e 5 5 e 5 5 5 e . 
+. . . . e e e e 5 5 e 5 5 5 5 e 
+. . e e 8 e e 8 e e 8 e e e e . 
+. . . e e e e e e e e e e . . . 
+. . . . e e e e e e e . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
